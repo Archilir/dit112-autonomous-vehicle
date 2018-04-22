9ee0ce736b44523ce72aa3f92b588e87b6281a68
Ahmad <gusalahmah@student.gu.se>
2018-04-22 22:12:51 +0200

    added collision detection for the back ultrasonic sensor

--- src/obstacledetection/obstacledetection.ino
+++ src/obstacledetection/obstacledetection.ino
@@ -10,11 +10,27 @@ int speedRight;
 // A Variable to store the direction inputed in the console
 String directionAndSpeed;
 
+//Front UltraSound sensor
+//const int TRIGGER_PIN_FRONT = ?;
+//const int ECHO_PIN_FRONT = ?;
+
+//Back UltraSound sensor
+const int TRIGGER_PIN_BACK = 5;
+const int ECHO_PIN_BACK = 4;
+
+
+SR04 FrontUSSensor;
+SR04 BackUSSensor;
+
+char switchVar;
+
 
 void setup() {
  Serial.begin(9600);
  Serial3.begin(9600);
  car.begin();
+ //FrontUSSensor.attach(TRIGGER_PIN_FRONT, ECHO_PIN_FRONT);
+  BackUSSensor.attach(TRIGGER_PIN_BACK, ECHO_PIN_BACK);
 
   Serial.println("Use the following to Control the car");
   Serial.println("                 ⇧                  ");
@@ -32,6 +48,8 @@ void setup() {
 }
 void loop() {
 
+  int frontDistance = FrontUSSensor.getDistance();
+  int backDistance = BackUSSensor.getDistance(); 
 
 if (Serial.available() > 0) {
     while (directionAndSpeed.length() < 1) {
@@ -47,6 +65,68 @@ if (Serial.available() > 0) {
     }
   }
 
+  switch (switchVar) {
+ 
+        car.setMotorSpeed(speedLeft, speedRight);
+
+      break;
+
+    case 's':
+
+      if (backDistance > 2 && backDistance < 20) {
+ 
+        car.setMotorSpeed(0, 0);
+
+      } else {
+ 
+        car.setMotorSpeed(speedLeft, speedRight);
+
+      }
+
+      break;
+
+    case 'q':
+
+        car.setMotorSpeed(speedLeft, speedRight);
+
+      break;
+
+    case 'e':
+
+        car.setMotorSpeed(speedLeft, speedRight);
+
+      break;
+
+    case 'z':
+
+      if (backDistance > 2 && backDistance < 20) {
+
+        car.setMotorSpeed(0, 0);
+
+
+      } else {
+  
+        car.setMotorSpeed(speedLeft, speedRight);
+
+      }
+
+      break;
+
+    case 'c':
+      if (backDistance > 2 && backDistance < 20) {
+
+        car.setMotorSpeed(0, 0);
+
+
+      } else {
+  
+        car.setMotorSpeed(speedLeft, speedRight);
+
+      }
+
+      break;
+  }
+
 
   char direction = directionAndSpeed.charAt(0);
 
@@ -57,11 +137,13 @@ if (Serial.available() > 0) {
 
       speedLeft = derive();
       speedRight = derive();
+      switchVar = 'w';
       car.setMotorSpeed(derive(), derive());
       break;
 
     case 'a' :
-    
+
+      switchVar = 'a';     
       car.setMotorSpeed(-derive(), derive());
       break;
 
@@ -69,11 +151,13 @@ if (Serial.available() > 0) {
     
       speedLeft = -derive();
       speedRight = -derive();
+      switchVar = 's';
       car.setMotorSpeed(-derive(), -derive());
       break;
 
     case 'd' :
 
+      switchVar = 'd';
       car.setMotorSpeed(derive(), -derive());
       
       break;
@@ -82,21 +166,23 @@ if (Serial.available() > 0) {
 
       speedLeft = derive() / 2;
       speedRight = derive();
+      switchVar = 'q';
       car.setMotorSpeed(derive() / 2, derive());
       break;
 
     case 'e' : 
 
-
       speedLeft = derive();
       speedRight = derive() / 2;
+      switchVar = 'e';
       car.setMotorSpeed(derive(), derive() / 2);
       break;
 
     case 'z' :
-
+    
       speedLeft = -derive() / 2;
       speedRight = -derive();
+      switchVar = 'z';
       car.setMotorSpeed(-derive() / 2, -derive());
       break;
 
@@ -105,11 +191,13 @@ if (Serial.available() > 0) {
 
       speedLeft = -derive();
       speedRight = -derive() / 2;
+      switchVar = 'c';
       car.setMotorSpeed(-derive(), -derive() / 2);
       break;
 
     case 'x' :
 
+      switchVar = 'w';
       car.setMotorSpeed(0, 0);
       break;
 
