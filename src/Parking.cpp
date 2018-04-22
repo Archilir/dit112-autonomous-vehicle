bba54d55f4dec5aca6ab7610a5387c8d4ff007d1
unknown <faticfilip98@gmail.com>
2018-04-22 17:19:33 +0200

    Tweaked reverse parking to acceptable state

--- src/Parking.cpp
+++ src/Parking.cpp
@@ -79,23 +79,36 @@ int Parking::getShortestDisplacement(){
   }
   else
   {
-    return displacement2;;
+    return displacement2;
   }
 }
 void Parking::reverseParking(){
   initialDisplacement = sensors -> getAngularDisplacement();
-  driver -> setAngle(35);
+  driver -> setAngle(45);
   driver -> setSpeed(-40);
   //the above 3 statements should be executed before this method is return
   //this is just for the sake of structuring it with ilja later
+
+  /*
+  * While the car is not aligned for parking, it will attempt to align itself while also maintaining distance
+  * from obstacles.
+  */
+
   if(getShortestDisplacement() <= 40 && !isReverseParking){
-    if(sensors -> getBRDistance() < 8){
+    if(sensors -> getBRDistance() > 0 && sensors -> getBRDistance() < 10){
       driver -> setAngle(0);
     }
     else{
-      driver -> setAngle(60);
+      driver -> setAngle(45);
     }
   }
+
+  /*
+   * After the target displacement is hit, theoretically the car should keep backing up
+   * until it is more or less inside the space.
+   * After that, it begins turning inside the space and it sets parking status to true.
+   */
+
   if((getShortestDisplacement() > 40)
       && (getShortestDisplacement() < 50)
       && !isReverseParking){
@@ -109,23 +122,31 @@ void Parking::reverseParking(){
     }
 
     else{
-     driver -> setAngle(-60);
+     driver -> setAngle(-45);
      isReverseParking = true;
     }
   }
-  //two end cases for reverse parking: hitting initial angular displacement
-  //or finding an obstacle behind itself
+
+  /*
+ * detects obstacles at the back of the car
+ * if the car is currently turning to the right, it starts turning to the left
+ * if the car is already turning to the left, it just sends it forwards
+ */
 
   if(sensors -> getBBDistance() > 0
   && sensors -> getBBDistance() < 21
   && isReverseParking){
     if(driver -> getAngle()< 0){
-      driver -> setAngle(60);
+      driver -> setAngle(45);
     }
-    else if(driver -> getAngle() > 60){
+    else if(driver -> getAngle() > 0){
       driver -> setSpeed(40);
     }
   }
+
+  /*
+   * If the car is aligned to its original displacement, it stops and flicks off the parking switch.
+   */
 
   if(getShortestDisplacement() < 2 && isReverseParking)
   {
