1ab86c08d73ebe8cd1b01f823797b316797526fa
VicRoman97 <38137876+VicRoman97@users.noreply.github.com>
2018-05-15 10:28:23 +0200

    V1 and V2 up

--- avoidance.ino
+++ avoidance.ino
@@ -5,68 +5,90 @@ Car car;
 #include <Smartcar.h>
 
 SR04 sonicA;
+SR04 sonicB;
+
 // Front Sensor, fill in with correct pin please
-const int TRIGGER_PIN_FR = --;
-const int ECHO_PIN_FR    = --;
+const int TRIGGER_PIN_FR = 45;
+const int ECHO_PIN_FR    = 43;
+const int TRIGGER_PIN_RR = 44;
+const int ECHO_PIN_RR    = 42;
 
+/*
+backwards 5
+4
+A8
+*/
+//#include <Servo.h>
+//int servoPin = -; //placeholder
+//Servo Servo1; //placeholder
 
-#include <Servo.h>
-int servoPin = -; //placeholder
-Servo Servo1; //placeholder
-
-int medianreadFront = sonicFR.getMedianDistance(5);
+int medianreadFront = sonicA.getMedianDistance(5);
+int medianreadRight = sonicB.getMedianDistance(5);
 
 void setup() {
   Serial.begin(9600);
   car.begin(); //initialize the car using the encoders and the gyro
-  sonicA.attach(TRIGGER_PIN_FR, ECHO_PIN_FR); //trigger pin, echo pin
-  Servo1.attach(servoPin); //placeholder
+  sonicA.attach(45, 43);
+  sonicB.attach(44, 42);
+  //Servo1.attach(servoPin); //placeholder
 }
 
 int distanceLeft = 0;
 int distanceRight = 0;
 int distanceCenter = 0;
+
 void loop() {
-    servo();
+  car.setSpeed(40);
     firstPart();
+    printDebug();
  }
 
 
 void firstPart() {
-if (distanceCenter >= 15)
-{} //givendistance do nothing
+
+if (medianreadFront >= 15)
+{}
+  //givendistance do nothing
 
   else
 {
 
-if (distanceLeft > distanceRight)
-  {moveleft()}
+if (medianreadRight <= 15)
+  {moveleft();}
 
-if (distanceRight > distanceLeft)
-  {moveright();}
-
-if (distanceLeft <= 10 && distanceCenter <= 10 || distanceRight <= 10 && distanceCenter <= 10)
+if (medianreadRight <=15 && medianreadFront <=15)
   {moveback();}
 
 }
 
 }
 
+void printDebug() {
+  Serial.println();
+}
+
 
 //second part, arguably
  void moveleft() {
-   car.setAngle(car.getAngle() - 10)
- }
+car.setAngle(90);
+car.setSpeed(10):
+
+}
 
   void moveback() {
-    car.setSpeed(-50);
-    car.setAngle(0);
+
+    car.setSpeed(-10):
+
   }
 
    void moveright() {
-     car.setAngle(car.getAngle() + 10);
+
    }
 
+
+
+
+/*
 //servo and measurments
 void servo() {
   Servo1.write(45);
@@ -77,10 +99,11 @@ void servo() {
   Servo1.write(135);
   distanceLeft = sonicA.getMedianDistance();
 }
-
+*/
 
 
 //stuff to be implemented, maybe(?)
+/*
   int minusspeed = 100-sonicFR.getMedianDistance(3);
   if (medianreadFront < 5 && 0 < medianreadFront ) {
   car.setAngle(-45);
@@ -89,3 +112,4 @@ void servo() {
  else
  car.setSpeed(0);
 }
+*/
